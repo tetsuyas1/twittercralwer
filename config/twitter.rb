9941c6fbdf62076f22c22a702b6d1b83cfd17945
LE MINH DUC <kemaodanh.2008@gmail.com>
2015-09-26 14:48:33 +0900

    add secrect json file

--- config/twitter.rb
+++ config/twitter.rb
@@ -2,12 +2,15 @@ require 'rubygems'
 require 'bundler/setup'
 
 require "twitter"
+require 'json'
+file = File.read('twitter_secrets.json')
+data_hash = JSON.parse(file)
 
 @twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "tfUBB6aOCHbveWXsHddYyDY8g"
-  config.consumer_secret     = "ffB9fv5NK0ByqNOHUeYZNQM7KwcbKhPrNM7iVhVINPiyLSPh9r"
-  config.access_token        = "201754724-F2yQ83rqDQum72rGmHdFr3xP0dbl4QN21BqsD1wA"
-  config.access_token_secret = "LQ0mdgWIB4H57Too7IhDA9m5eOp6IxaNDvt1lPecMs6X6"
+  config.consumer_key        =  data_hash['CONSUMER_KEY']
+  config.consumer_secret     =  data_hash['CONSUMER_SECRECT']
+  config.access_token        =  data_hash['ACCESS_TOKEN']
+  config.access_token_secret =  data_hash['ACCESS_TOKEN_SECRECT'] 
 end
 
 def user_timeline(user_id)
